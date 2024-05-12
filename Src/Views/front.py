c1b2aa00b2725b0dff27d396b50be993a6b2d160
Jhon1551 <jhow1551@gmail.com>
2024-05-11 23:06:36 -0300

    Import Atualizados

--- Src/Views/front.py
+++ Src/Views/front.py
@@ -1,5 +1,4 @@
-from tkinter import *
-from customtkinter import CTkScrollableFrame
+from tkinter import Label, Button, Text, Frame, Tk
 from os.path import join, abspath,dirname
 from PIL import Image, ImageTk
 from tkinter import font
@@ -24,10 +23,12 @@ animado = ImageTk.PhotoImage(animado) # avatar.configure(image=meio_animado)
 meio_animado = Image.open(meio_animado).resize((95,95)) # Essa é a imagem inicial e precisa ser vinculada ao usuario começar a digitar na "entry_buscar"
 meio_animado = ImageTk.PhotoImage(meio_animado) # usando bind.
 
+
+aguardando = "Me dê um exemplo de anime, irei procurar e te mostrar recomendações excelentes" # retorna ao normal ao cliente clicar dentro da Text
 textao = 'Doli1\ndoli3\ndoli4\ndoli5\ndoli6\ndoli7\ndoli8\ndoli9\ndoli10'
 procurando = "Aguarde um pouco, estou procurando pelas melhores recomendações" #Isso será usado para trocar o texto do topo após clicar no botão
 #usando o --> apresentar .configure(text=procurando)
-aguardando = "Me dê um exemplo de anime, irei procurar e te mostrar recomendações excelentes" # retorna ao normal ao cliente clicar dentro da Text
+
 
 frame_a = Frame(tela,bg="#7b7b7b")
 frame_a.place(relx=0.02,rely=0.05,relheight=0.3,relwidth=0.96)
@@ -44,8 +45,7 @@ fonte_maior = font.Font(size=14)  # Definir o tamanho da fonte como 14
 entry_buscar = Text(frame_a,font=("Arial",20),height=3,width=74,wrap='word') # Aqui é onde o usuario digita a procura por indicação
 entry_buscar.grid(row=1,column=1,sticky='nswe')
 
-entry_buscar.insert("1.0",textao)
-entry_buscar.bind("<Button-1>",avatar.configure(image=animado))
+entry_buscar.bind("<Button-1>",avatar.configure(image=meio_animado))
 
 confirmar_busca = Button(frame_a,text="Encontrar",bg="#f4ab55",height=5) # esse é o botão que clica e envia os dados pro frame_b
 confirmar_busca.grid(row=1,column=2,padx=10,sticky='nswe')
